ff8049dc650e723d273020fc85364528d3cfec42
cong <1391941195@qq.com>
2020-04-22 13:52:39 +0800

    添加articels文章表迁移

---
+++ database/migrations/2020_04_22_054629_create_articels_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articels', function (Blueprint $table) {
+            $table->id()->comment('文章表主键');
+            $table->unsignedTinyInteger('category_id')->default(0)->comment('分类id');
+            $table->string('title')->default('')->comment('标题');
+            $table->string('slug')->default('')->comment('sulg');
+            $table->string('author')->default('')->comment('作者');
+            $table->mediumText('markdown')->comment('markdown文章内容');
+            $table->mediumText('html')->comment('markdown转的html页面');
+            $table->string('description')->default()->comment('描述');
+            $table->string('keywords')->default('')->comment('关键词');
+            $table->string('cover')->default('')->comment('封面图');
+            $table->unsignedTinyInteger('is_top')->default(0)->comment('是否置顶 1是 0否');
+            $table->integer('views')->unsigned()->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articels');
+    }
+}
